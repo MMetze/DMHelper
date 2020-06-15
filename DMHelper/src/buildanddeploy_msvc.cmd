37cd131a1728d183292bc8ea676d2cfcf8a24be4
DM Helper <44725987+dm-helper@users.noreply.github.com>
2020-06-15 12:11:23 +0200

    Updated Installer
    Added installer paths, resources and scripts
    Updated build script to start installer and use installer paths
    Added GPL v3 license text
    Updated icon

--- DMHelper/src/buildanddeploy_msvc.cmd
+++ DMHelper/src/buildanddeploy_msvc.cmd
@@ -4,26 +4,37 @@
 if '%choice%'=='n' goto end
 if not '%choice%'=='y' goto start
 
-set PATH=C:\Qt\5.14.2\msvc2017_64\bin;C:\Qt\Tools\QtCreator\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+set QT_DIR=C:\Qt
+set QT_VERSION=5.14.2
+set SEVENZIP_APP=C:\Program Files\7-Zip\7z
+
+rem set PATH=C:\Qt\5.14.2\msvc2017_64\bin;C:\Qt\Tools\QtCreator\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+set PATH=%QT_DIR%\%QT_VERSION%\msvc2017_64\bin;%QT_DIR%\Tools\QtInstallerFramework\3.2\bin;%QT_DIR%\Tools\QtCreator\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
 
 rmdir /s /q ..\bin64
 mkdir ..\bin64
-mkdir ..\bin64\bestiary
-mkdir ..\bin64\doc
-mkdir ..\bin64\pkgconfig
-mkdir ..\bin64\plugins
-mkdir ..\bin64\resources
-mkdir ..\bin64\resources\tables
+mkdir ..\bin64\config
+mkdir ..\bin64\packages
+mkdir ..\bin64\packages\com.dmhelper.app
+mkdir ..\bin64\packages\com.dmhelper.app\meta
+mkdir ..\bin64\packages\com.dmhelper.app\data
+mkdir ..\bin64\packages\com.dmhelper.app\data\bestiary
+mkdir ..\bin64\packages\com.dmhelper.app\data\doc
+mkdir ..\bin64\packages\com.dmhelper.app\data\pkgconfig
+mkdir ..\bin64\packages\com.dmhelper.app\data\plugins
+mkdir ..\bin64\packages\com.dmhelper.app\data\resources
+mkdir ..\bin64\packages\com.dmhelper.app\data\resources\tables
+xcopy /s .\installer\* ..\bin64\*
 
 cd ..
 
 rem Uncomment the following line to skip actually building the SW
-rem goto skip_build
+goto skip_build
 
 rmdir /s /q .\build-64_bit-release
 mkdir build-64_bit-release
 cd build-64_bit-release
-@call vcvarsall.bat x64
+call vcvarsall.bat x64
 @echo on
 qmake.exe ..\src\DMHelper.pro -spec win32-msvc "CONFIG+=qtquickcompiler" 
 jom.exe /d /f Makefile qmake_all
@@ -35,30 +46,31 @@ cd build-64_bit-release
 
 :build_done
 
-xcopy .\release\DMHelper.exe ..\bin64\
-xcopy C:\Qt\5.14.2\msvc2017_64\bin\Qt5Xml.dll ..\bin64\
-xcopy ..\src\binsrc\* ..\bin64\*
-xcopy /s ..\src\bestiary\* ..\bin64\resources\*
-xcopy /s ..\src\doc\* ..\bin64\doc\*
-xcopy /s ..\src\binsrc\pkgconfig\* ..\bin64\pkgconfig\*
-xcopy /s ..\src\binsrc\plugins\* ..\bin64\plugins\*
-xcopy /s ..\src\resources\* ..\bin64\resources\*
+xcopy .\release\DMHelper.exe ..\bin64\packages\com.dmhelper.app\data\
+xcopy %QT_DIR%\%QT_VERSION%\msvc2017_64\bin\Qt5Xml.dll ..\bin64\packages\com.dmhelper.app\data\
+xcopy ..\src\binsrc\* ..\bin64\packages\com.dmhelper.app\data\*
+xcopy /s ..\src\bestiary\* ..\bin64\packages\com.dmhelper.app\data\resources\*
+xcopy /s ..\src\doc\* ..\bin64\packages\com.dmhelper.app\data\doc\*
+xcopy /s ..\src\binsrc\pkgconfig\* ..\bin64\packages\com.dmhelper.app\data\pkgconfig\*
+xcopy /s ..\src\binsrc\plugins\* ..\bin64\packages\com.dmhelper.app\data\plugins\*
+xcopy /s ..\src\resources\* ..\bin64\packages\com.dmhelper.app\data\resources\*
 
-windeployqt --compiler-runtime --no-opengl-sw --no-angle --no-svg ..\bin64
+windeployqt --compiler-runtime --no-opengl-sw --no-angle --no-svg ..\bin64\packages\com.dmhelper.app\data
 
+rem Create the installer
 cd ..\bin64
-rem NO LONGER NEEDED as this is now done in the appdata directory: copy NUL DMHelper.log
-
-rem del vc_redist.x64.exe
-
-"C:\Program Files\7-Zip\7z" a -tzip archive.zip *
+binarycreator -c config\config.xml -p packages "DM Helper 64-bit release Installer"
 cd ..
-move bin64\archive.zip .\archive.zip
+move ".\bin64\DM Helper 64-bit release Installer.exe" ".\DM Helper 64-bit release Installer.exe"
+
+rem Create the zip-file distribution
+"%SEVENZIP_APP%" a -tzip archive.zip .\bin64\packages\com.dmhelper.app\data\*
 del "DM Helper 64-bit release.zip"
 rename archive.zip "DM Helper 64-bit release.zip"
 
+rem Create the source distribution
 del src.zip
-"C:\Program Files\7-Zip\7z" a -tzip src.zip src\*
+"%SEVENZIP_APP%" a -tzip src.zip .\src\*
 
 :end
 pause
