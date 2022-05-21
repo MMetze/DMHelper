45446d4dff4453463994b0395e99cec3c71385d5
DM Helper <44725987+dm-helper@users.noreply.github.com>
2022-05-21 11:00:00 +0200

    Updated version for v2.4.2

--- DMHelper/src/dmversion.h
+++ DMHelper/src/dmversion.h
@@ -7,7 +7,7 @@ namespace DMHelper
 {
     const int DMHELPER_MAJOR_VERSION = 2;
     const int DMHELPER_MINOR_VERSION = 4;
-    const int DMHELPER_ENGINEERING_VERSION = 0;
+    const int DMHELPER_ENGINEERING_VERSION = 2;
 
     const int CAMPAIGN_MAJOR_VERSION = 2;
     const int CAMPAIGN_MINOR_VERSION = 1;
