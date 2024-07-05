5bc13485c2b7212308a627f13b8396ce459c47e6
DM Helper <44725987+dm-helper@users.noreply.github.com>
2024-07-05 13:31:18 +0200

    Update videoplayerglscreenshot.cpp

--- DMHelper/src/videoplayerglscreenshot.cpp
+++ DMHelper/src/videoplayerglscreenshot.cpp
@@ -183,7 +183,7 @@ bool VideoPlayerGLScreenshot::startPlayer()
 #ifdef Q_OS_WIN
     localizedVideoFile.replace("/", "\\\\");
 #endif
-#if defined(Q_OS_WIN64) || defined(Q_OS_MAC)
+#if defined(Q_OS_WIN64) || defined(Q_OS_MAC) || defined(Q_OS_LINUX)
     _vlcMedia = libvlc_media_new_path(localizedVideoFile.toUtf8().constData());
 #else
     _vlcMedia = libvlc_media_new_path(DMH_VLC::vlcInstance(), localizedVideoFile.toUtf8().constData());
@@ -194,7 +194,7 @@ bool VideoPlayerGLScreenshot::startPlayer()
         return false;
     }
 
-#if defined(Q_OS_WIN64) || defined(Q_OS_MAC)
+#if defined(Q_OS_WIN64) || defined(Q_OS_MAC) || defined(Q_OS_LINUX)
     _vlcPlayer = libvlc_media_player_new_from_media(DMH_VLC::vlcInstance(), _vlcMedia);
 #else
     _vlcPlayer = libvlc_media_player_new_from_media(_vlcMedia);
